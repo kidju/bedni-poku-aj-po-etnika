829ff2cfa078305566a4d2eae2aa75d89b55b171
Đurđa P <djurdjapetkovic01@gmail.com>
2017-06-30 13:54:14 +0200

    Add files via upload

---
+++ komentar.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+print("Hello world!")
+#phhh komentari hehe
+print("Ovo je druga linija!")
+"""komentarišemmmm
+i dalje to radim
+propala sam
+ne znam računarsku lin
+"""
