670e65ed1d96add5cbf07dfce474d5b42c97c97e
Đurđa <djurdjapetkovic01@gmail.com>
2017-07-02 16:07:37 +0200

    Add files via upload

---
+++ domaci2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+recnik = {"hladno": {"sinonimi":["ledeno", "studeno"], "antonimi":["vruce", "toplo"]}, "sareno":{"sinonimi":["raznobojno", "zivopisno",], "antonimi":["jednoliko", "jednobojno",]}, "komplikovano":{"sinonimi":["tesko", "programiranje"], "antonimi":["jednostavno", "lako"]}}
+
+print(recnik["hladno"]["sinonimi"])
+print(recnik["sareno"]["antonimi"])
+print(recnik["komplikovano"]["sinonimi"][1])
+print(recnik["komplikovano"]["antonimi"][0])
+
+#samo pola jer ostalo ne kapiram :)
